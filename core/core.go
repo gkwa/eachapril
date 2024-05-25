3589376f9eb74a9d3cc2ccdb165e4cba79d5f16e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-25 00:51:54 -0700

    failing

--- core/core.go
+++ core/core.go
@@ -3,12 +3,15 @@ package core
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/blevesearch/bleve/v2"
 )
 
 func Run() {
 	mapping := bleve.NewIndexMapping()
+	mapping.DefaultField = "content"
 
 	index, err := bleve.New("example.bleve", mapping)
 	if err != nil {
@@ -16,25 +19,44 @@ func Run() {
 	}
 	defer index.Close()
 
-	data := struct {
-		Title string
-		Body  string
-	}{
-		Title: "Hello World",
-		Body:  "This is a sample document.",
-	}
+	markdownDir := "markdown"
+	err = filepath.Walk(markdownDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-	err = index.Index("doc1", data)
+		if !info.IsDir() && filepath.Ext(path) == ".md" {
+			content, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			err = index.Index(path, string(content))
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	query := bleve.NewMatchQuery("hello")
-	search := bleve.NewSearchRequest(query)
-	searchResults, err := index.Search(search)
+	query1 := bleve.NewMatchQuery("Go")
+	query2 := bleve.NewMatchQuery("code")
+
+	conjunctionQuery := bleve.NewConjunctionQuery(query1, query2)
+
+	searchRequest := bleve.NewSearchRequest(conjunctionQuery)
+
+	searchResults, err := index.Search(searchRequest)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(searchResults)
+	fmt.Println("Search Results:")
+	for _, hit := range searchResults.Hits {
+		fmt.Printf("Document: %s, Score: %f\n", hit.ID, hit.Score)
+	}
 }
