f0cad038cffd3a1264c394d150a4d5fc32006da1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-25 00:23:07 -0700

    ok

--- core/core.go
+++ core/core.go
@@ -1,7 +1,40 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func Hello() {
-	fmt.Println("Hello, World!")
+	"github.com/blevesearch/bleve/v2"
+)
+
+func Run() {
+	mapping := bleve.NewIndexMapping()
+
+	index, err := bleve.New("example.bleve", mapping)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer index.Close()
+
+	data := struct {
+		Title string
+		Body  string
+	}{
+		Title: "Hello World",
+		Body:  "This is a sample document.",
+	}
+
+	err = index.Index("doc1", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := bleve.NewMatchQuery("hello")
+	search := bleve.NewSearchRequest(query)
+	searchResults, err := index.Search(search)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(searchResults)
 }
